54eb910e9fd10ffc6ca2c7ea9acd82802ccb7c3a
nicraM <git.nicram@gmail.com>
2013-01-18 20:41:58 +0100

    Base for all EFM32 projects. Including env for IAR and VisialStudio 2012E

---
+++ main.c
@@ -0,0 +1,15 @@
+#ifdef EFM32GG990F1024
+#include "efm32gg990f1024.h"
+#else //PROJECT IN VISUAL STUDIO 2012 EXPRESS
+#include "include\efm32gg990f1024.h"
+#include "include\core_cm3.h"
+#include "include\efm32gg_cmu.h"
+#include "include\efm32gg_gpio.h"
+#include "include\efm32gg_emu.h"
+#include "include\efm32gg_msc.h"
+#include "include\efm32gg_rtc.h"
+#endif
+
+int main()
+{
+}
