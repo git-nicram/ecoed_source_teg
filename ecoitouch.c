e3065ee80e8e1648f954715f07518330f5214ec7
nicraM <git.nicram@gmail.com>
2013-01-23 17:35:05 +0100

    BUG001: VS2012 Fix + small changes in event handlers

--- ecoitouch.c
+++ ecoitouch.c
@@ -10,18 +10,196 @@
  */
 
 
+
 #ifdef EFM32GG990F1024
 #include "efm32gg990f1024.h"
-#include "ecoitouch.h
 #else /* FOR PROJECT IN VISUAL STUDIO 2012 EXPRESS */
-#include "ecoitouch.h"
 #include "include\efm32gg990f1024.h"
 #endif
 
 
+/***************/
+/* DEFINITIONS */
+/***************/
 
-/* I N T E R R U P T   F U N C T I O N S */ 
+#define EN_DIS_ALL_INTERRUPTS   0xFFFFFFFF
+#define PAGE0                   0UL
+#define PAGE1                   1UL
 
+#define HFCORECLK               32768UL
+#define MINUTE_HOUR             60UL
+#define WAKE_UP_TIME_SECONDS(x) (x * HFCORECLK - 1)
+#define WAKE_UP_TIME_MINUTES(x) (x * HFCORECLK * MINUTE_HOUR - 1)
+#define WAKE_UP_TIME_HOURS(x)   (x * HFCORECLK * MINUTE_HOUR * MINUTE_HOUR - 1)
+
+#define VOLTAGE_LEVEL_LOW       2.4F
+#define VOLTAGE_LEVEL_HIGH      3.0F
+#define VOLTAGE_MAGIC_1_CONST   29.411764F
+#define VOLTAGE_MAGIC_2_CONST   49.029411F
+#define THRESHOLD_VOLTAGE(x)   ((uint32_t)((x * VOLTAGE_MAGIC_1_CONST) \
+	                                          - VOLTAGE_MAGIC_2_CONST))
+
+/************************/
+/* STRUCTURES AND ENUMS */
+/************************/
+
+/*******************/
+/* GLOBAL VARIABLE */
+/*******************/
+
+__IO uint32_t g_minutes = 0;
+
+
+/*********************/
+/* CONSTANT VARIABLE */
+/*********************/
+
+
+/**********************/
+/* EXTERNAL FUNCTIONS */
+/**********************/
+
+/******************/
+/* EVENT HANDLERS */
+/******************/
+
+static void RTC_EventHandler(void)
+{
+    /* Clear XXX interrupt */
+    
+    /* Clear pending IRQ */
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)RTC_IRQn & 0x1F));
+	
+    /* Event handling */
+
+}
+
+
+
+#ifdef __ICCARM__
+__ramfunc __STATIC_INLINE
+#endif
+void VCMP_EventHandler(void)
+{
+    /* Check interrupt source */
+    if (!(VCMP->IF & VCMP_IF_EDGE))
+        return;
+
+    /* Clear interrupt flag */
+    VCMP->IFC &= ~VCMP_IFC_EDGE;     
+         
+    /* Dissable all interrupts */
+    NVIC->ICER[PAGE0] = EN_DIS_ALL_INTERRUPTS;
+    NVIC->ICER[PAGE1] = EN_DIS_ALL_INTERRUPTS;
+    
+    /* Clear pending IRQ */
+    NVIC->ICPR[(uint32_t)PAGE1] |= (0 << ((uint32_t)VCMP_IRQn & 0x1F));
+    
+    /* Disable VCMP and make reconfiguration */
+    VCMP->CTRL &= ~VCMP_CTRL_EN;
+        
+    VCMP->INPUTSEL |= THRESHOLD_VOLTAGE(VOLTAGE_LEVEL_HIGH) &
+                      _VCMP_INPUTSEL_TRIGLEVEL_MASK;
+
+    /* Enable VCMP */
+    VCMP->CTRL |= VCMP_CTRL_EN;
+
+    /* Wait until VCMP warmed-up */
+    while(!(VCMP->CTRL & VCMP_CTRL_INACTVAL));
+
+    /* Low power mode */
+    VCMP->INPUTSEL |= VCMP_INPUTSEL_LPREF;
+
+    /* initialize and configure interrupts */
+    VCMP->IEN |= VCMP_IEN_EDGE;
+    
+    /* Enable VCMP interrupt */
+    NVIC->ISER[(uint32_t)PAGE1] |= (1 << (uint32_t)(VCMP_IRQn & 0x1F));
+	
+    /* Enter stop mode (EM3)- disable LFRCO and enable ULFRCO */
+    CMU->LFCLKSEL |= CMU_LFCLKSEL_LFAE       |
+                     CMU_LFCLKSEL_LFBE;
+    CMU->LFCLKSEL &= ~_CMU_LFCLKSEL_LFA_MASK &
+                     ~_CMU_LFCLKSEL_LFB_MASK;
+
+    CMU->OSCENCMD |= CMU_OSCENCMD_LFRCODIS;
+
+    /* Disable pending event notification */
+    SCB->SCR &= ~SCB_SCR_SEVONPEND_Msk;
+
+    /* Sweet dreams */
+    __WFI();
+}
+
+
+
+#ifdef __ICCARM__
+__ramfunc __STATIC_INLINE
+#endif
+void GPIO_EVEN_EventHandler(void)
+{
+    /* Clear XXX interrupt */
+    
+    /* Clear pending IRQ */
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)GPIO_EVEN_IRQn & 0x1F));
+
+    /* Event handling */
+
+}
+
+
+
+#ifdef __ICCARM__
+__ramfunc __STATIC_INLINE
+#endif
+void GPIO_ODD_EventHandler(void)
+{
+    /* Clear XXX interrupt */
+    
+    /* Clear pending IRQ */
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)GPIO_ODD_IRQn & 0x1F));
+
+    /* Event handling */
+    
+}
+
+
+
+#ifdef __ICCARM__
+__ramfunc __STATIC_INLINE
+#endif
+void LEUART0_EventHandler(void)
+{
+    /* Clear XXX interrupt */
+    
+    /* Clear pending IRQ */
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)LEUART0_IRQn & 0x1F));
+    
+    /* Event handling */
+
+}
+
+
+
+/******************************************************************************
+Function that disables enabled by default clock (HFRCO) and enables LFRCO
+******************************************************************************/
+#ifdef __ICCARM__
+__ramfunc __STATIC_INLINE
+#endif
+void enableLFRCO(void)
+{
+    CMU->OSCENCMD |= CMU_OSCENCMD_LFRCOEN;
+    while(!(CMU->STATUS & _CMU_STATUS_LFRCORDY_MASK));
+    CMU->CMD |= CMU_CMD_HFCLKSEL_LFRCO;
+    CMU->OSCENCMD |= CMU_OSCENCMD_HFRCODIS;
+}
+
+
+
+/***********************/
+/* INTERRUPT FUNCTIONS */ 
+/***********************/
 
 void SysTick_Handler(void)
 {
@@ -54,7 +232,7 @@ void VCMP_IRQHandler(void)
         
     VCMP->INPUTSEL |= THRESHOLD_VOLTAGE(VOLTAGE_LEVEL_LOW) &
                       _VCMP_INPUTSEL_TRIGLEVEL_MASK;
-
+	
     /* Enable VCMP */
     VCMP->CTRL |= VCMP_CTRL_EN;
     
@@ -68,7 +246,7 @@ void VCMP_IRQHandler(void)
     VCMP->IEN |= VCMP_IEN_EDGE;
     
     /* Enable VCMP interrupt */
-    NVIC->ISER[(uint32_t)PAGE1] |= (1 << (uint32_t)VCMP_IRQn & 0x1F); 
+    NVIC->ISER[(uint32_t)PAGE1] |= (1 << (uint32_t)(VCMP_IRQn & 0x1F)); 
 }
 
 void GPIO_EVEN_IRQHandler(void)
@@ -95,8 +273,9 @@ void LEUART0_IRQHandler(void)
     
 }
 
-
-/* L O C A L   F U N C T I O N S */
+/*******************/
+/* LOCAL FUNCTIONS */
+/*******************/
 
 /******************************************************************************
  After reset uC is running on HFRCO(14MHz) - 2us startup time 
@@ -273,8 +452,10 @@ static void initNVIC(void)
                                   (1 << (uint32_t)GPIO_EVEN_IRQn & 0x1F); 
 }
 
+/*****************/
+/* MAIN FUNCTION */
+/*****************/
 
-/* M A I N   F U N C T I O N */
 int main(void)
 {
     /* Chip fixes provided by energymicro in errata 
