7f7241693300fb0f9c387720cf1a7e2dd5287985
RokM <rokm@viessmann.com>
2013-01-25 15:11:31 +0100

    Some changes in code + first version of uC port configuration

--- ecoitouch.c
+++ ecoitouch.c
@@ -1,7 +1,22 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  *
- * 0.0.5  xx.xx.2013    nicraM    
- * 0.0.4  xx.xx.2013    nicraM    GPIO configuration and handling 
+ * 1.0.0  xx.xx.2013    nicraM    Official release. Rev A
+ * * *
+ * 0.6.0  xx.xx.2013    nicraM    Fixes and changes for LEUART
+ * 0.5.0  xx.xx.2013    nicraM    Fixes and changes for RTC
+ * 0.4.0  xx.xx.2013    nicraM    Fixes and changes for GPIO
+ * 0.3.0  xx.xx.2013    nicraM    Fixes and changes for SysTick and VCMP
+ *                                - SysTick removed since not available in
+ *                                deep sleep. 
+ * 0.2.0  xx.xx.2013    nicraM    Fixes and changes for sleep modes, event and
+ *                                interrupt handlings
+ * 0.1.0  xx.xx.2013    nicraM    Fixes and changes for clock and reset 
+ *                                detection
+ * * *
+ * 0.0.7  xx.xx.2013    nicraM    
+ * 0.0.6  xx.xx.2013    nicraM    LEUART configuration and functionality
+ * 0.0.5  xx.xx.2013    nicraM    RTC configuration
+ * 0.0.4  28.01.2013    nicraM    GPIO configuration and handling 
  * 0.0.3  23.01.2013    nicraM    SysTick and VCMP configuration
  * 0.0.2  23.01.2013    nicraM    Deep sleep mode configuration. Event and
  *                                interrupt configuration and basic handling
@@ -28,16 +43,17 @@
 
 #define HFCORECLK               32768UL
 #define MINUTE_HOUR             60UL
-#define WAKE_UP_TIME_SECONDS(x) (x * HFCORECLK - 1)
-#define WAKE_UP_TIME_MINUTES(x) (x * HFCORECLK * MINUTE_HOUR - 1)
-#define WAKE_UP_TIME_HOURS(x)   (x * HFCORECLK * MINUTE_HOUR * MINUTE_HOUR - 1)
+#define WAKE_UP_TIME_SECONDS(x) (uint32_t)(x * HFCORECLK - 1)
+#define WAKE_UP_TIME_MINUTES(x) (uint32_t)(x * HFCORECLK * MINUTE_HOUR - 1)
+#define WAKE_UP_TIME_HOURS(x)   (uint32_t)(x * HFCORECLK * MINUTE_HOUR * \
+                                                           MINUTE_HOUR - 1)
 
 #define VOLTAGE_LEVEL_LOW       2.4F
 #define VOLTAGE_LEVEL_HIGH      3.0F
 #define VOLTAGE_MAGIC_1_CONST   29.411764F
 #define VOLTAGE_MAGIC_2_CONST   49.029411F
-#define THRESHOLD_VOLTAGE(x)   ((uint32_t)((x * VOLTAGE_MAGIC_1_CONST) \
-	                                          - VOLTAGE_MAGIC_2_CONST))
+#define THRESHOLD_VOLTAGE(x)    (uint32_t)((x * VOLTAGE_MAGIC_1_CONST) \
+	                                          - VOLTAGE_MAGIC_2_CONST)
 
 /************************/
 /* STRUCTURES AND ENUMS */
@@ -68,7 +84,7 @@ static void RTC_EventHandler(void)
     /* Clear XXX interrupt */
     
     /* Clear pending IRQ */
-    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)RTC_IRQn & 0x1F));
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << (uint32_t)(RTC_IRQn & 0x1F));
 	
     /* Event handling */
 
@@ -93,7 +109,7 @@ void VCMP_EventHandler(void)
     NVIC->ICER[PAGE1] = EN_DIS_ALL_INTERRUPTS;
     
     /* Clear pending IRQ */
-    NVIC->ICPR[(uint32_t)PAGE1] |= (0 << ((uint32_t)VCMP_IRQn & 0x1F));
+    NVIC->ICPR[(uint32_t)PAGE1] |= (0 << (uint32_t)(VCMP_IRQn & 0x1F));
     
     /* Disable VCMP and make reconfiguration */
     VCMP->CTRL &= ~VCMP_CTRL_EN;
@@ -141,7 +157,7 @@ void GPIO_EVEN_EventHandler(void)
     /* Clear XXX interrupt */
     
     /* Clear pending IRQ */
-    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)GPIO_EVEN_IRQn & 0x1F));
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << (uint32_t)(GPIO_EVEN_IRQn & 0x1F));
 
     /* Event handling */
 
@@ -157,7 +173,7 @@ void GPIO_ODD_EventHandler(void)
     /* Clear XXX interrupt */
     
     /* Clear pending IRQ */
-    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)GPIO_ODD_IRQn & 0x1F));
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << (uint32_t)(GPIO_ODD_IRQn & 0x1F));
 
     /* Event handling */
     
@@ -173,7 +189,7 @@ void LEUART0_EventHandler(void)
     /* Clear XXX interrupt */
     
     /* Clear pending IRQ */
-    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << ((uint32_t)LEUART0_IRQn & 0x1F));
+    NVIC->ICPR[(uint32_t)PAGE0] |= (0 << (uint32_t)(LEUART0_IRQn & 0x1F));
     
     /* Event handling */
 
@@ -323,9 +339,9 @@ static void initCMU(void)
 
 
 /******************************************************************************
-Basic initialization of SysTick CORTEX-M3 timer. By default it generates
-exception every 1 minute. Clock source is HFCORECLK.
-NOTE:
+ Basic initialization of SysTick CORTEX-M3 timer. By default it generates
+ exception every 1 minute. Clock source is HFCORECLK.
+ NOTE:
     - WAKE_UP_TIME_SECONDS(x) 
     - WAKE_UP_TIME_MINUTES(x) 
     - WAKE_UP_TIME_HOURS(x)
@@ -357,7 +373,10 @@ static void initRTC(void)
 
 
 /******************************************************************************
-TODO:
+ Configuration of voltage comparator to prevent uC brown-out or power reset.
+ The lower threshold is set to 2.4V by default.
+ NOTE:
+     - THRESHOLD_VOLTAGE(x)
 ******************************************************************************/
 static void initVCMP(void)
 {
@@ -401,13 +420,17 @@ static void initLEUART(void)
 }
 
 /******************************************************************************
-TODO:
+ Base configuration of GPIO pins according to board design.
+ NOTE:
+    - Pin PA0 as input with pull-down resistor
+    - Pin PA1 as input with pull-down resistor
+    - Pin PB8 as output with pull
+ TODO:
+    - configuration may be changed during HW prototyping
 ******************************************************************************/
 static void initGPIO(void)
 {
-    /* interrupt section */
-    
-    /* general configuration */
+    GPIO
 }
 
 
@@ -440,16 +463,16 @@ static void initNVIC(void)
     /* Set interrupts enable in NVIC */
 
                                      /* RTC interrupt */
-    NVIC->ISER[(uint32_t)PAGE0] |= (1 << (uint32_t)RTC_IRQn & 0x1F)  | 
+    NVIC->ISER[(uint32_t)PAGE0] |= (1 << (uint32_t)(RTC_IRQn & 0x1F))      | 
 
                                      /* LEUART interrupt */ 
-                                   (1 << (uint32_t)LEUART0_IRQn & 0x1F)  |
+                                   (1 << (uint32_t)(LEUART0_IRQn & 0x1F))  |
 
                                      /* GPIO interrupt for odd */ 
-                                  (1 << (uint32_t)GPIO_ODD_IRQn & 0x1F) |  
+                                  (1 << (uint32_t)(GPIO_ODD_IRQn & 0x1F))  |  
 	   
                                      /* GPIO interrupt for even */
-                                  (1 << (uint32_t)GPIO_EVEN_IRQn & 0x1F); 
+                                  (1 << (uint32_t)(GPIO_EVEN_IRQn & 0x1F)); 
 }
 
 /*****************/
@@ -502,19 +525,19 @@ int main(void)
         enableLFRCO();
 
         /* Read pending interrupts and event handling */
-        if(NVIC->ISPR[(uint32_t)PAGE0] & ((uint32_t)RTC_IRQn & 0x1F)) 
+        if (NVIC->ISPR[(uint32_t)PAGE0] & (uint32_t)(RTC_IRQn & 0x1F)) 
             RTC_EventHandler();
 
-        if(NVIC->ISPR[(uint32_t)PAGE1] & ((uint32_t)VCMP_IRQn & 0x1F)) 
+        if (NVIC->ISPR[(uint32_t)PAGE1] & (uint32_t)(VCMP_IRQn & 0x1F))
             VCMP_EventHandler();
 
-        if(NVIC->ISPR[(uint32_t)PAGE0] & ((uint32_t)LEUART0_IRQn & 0x1F)) 
+        if (NVIC->ISPR[(uint32_t)PAGE0] & (uint32_t)(LEUART0_IRQn & 0x1F))
             LEUART0_EventHandler();
 
-        if(NVIC->ISPR[(uint32_t)PAGE0] & ((uint32_t)GPIO_ODD_IRQn & 0x1F)) 
+        if (NVIC->ISPR[(uint32_t)PAGE0] & (uint32_t)(GPIO_ODD_IRQn & 0x1F))
             GPIO_ODD_EventHandler();
 
-        if(NVIC->ISPR[(uint32_t)PAGE0] & ((uint32_t)GPIO_EVEN_IRQn & 0x1F)) 
+        if (NVIC->ISPR[(uint32_t)PAGE0] & (uint32_t)(GPIO_EVEN_IRQn & 0x1F))
             GPIO_EVEN_EventHandler();
     }
 }
